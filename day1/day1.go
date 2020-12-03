ef48a4bea9c6091d19de7fd34e0687a488cd373e
Fabio Nicolini <fabionicolini48@gmail.com>
2020-12-03 15:24:13 +0100

    First commit. Add day1 and day2 puzzles

---
+++ day1/day1.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	puzzle1()
+	puzzle2()
+}
+
+func puzzle1() {
+
+	fmt.Println("Day 1, puzzle 1.\n\n Find the two entries that sum to 2020; what do you get if you multiply them together?")
+
+	//Read file content in slice of bytes
+	content, err := ioutil.ReadFile("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/*
+		Cast slice of bytes to a string (which contains the whole file) and then
+		use the split function to create a slice using a separator (newline)
+	*/
+	lines := strings.Split(string(content), "\n")
+
+	/*
+		For each element x of index i of the slice, loop from index i + 1 and check every
+		 sum. Once the correct pair is found, print the result and exit the program.
+	*/
+	for i, a := range lines {
+		for _, b := range lines[i+1:] {
+			x, err := strconv.Atoi(a)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			y, err := strconv.Atoi(b)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if (x + y) == 2020 {
+				z := x * y
+
+				fmt.Printf("The two entries that sum to 2020 are %v and %v. Multiplying them together produces %v * %v = %v\n", x, y, x, y, z)
+
+				return
+			}
+
+		}
+	}
+
+}
+
+func puzzle2() {
+
+	fmt.Println("\nDay 1, puzzle 2.\n\n What is the product of the three entries that sum to 2020?")
+
+	//Read file content in slice of bytes
+	content, err := ioutil.ReadFile("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/*
+		Cast slice of bytes to a string (which contains the whole file) and then
+		use the split function to create a slice using a separator (newline)
+	*/
+	lines := strings.Split(string(content), "\n")
+
+	/*
+		For each element x of index i of the slice, loop from index i + 1 and check every
+		 sum. Once the correct pair is found, print the result and exit the program.
+	*/
+	for i, a := range lines {
+		for j, b := range lines[i+1:] {
+			for _, c := range lines[j+1:] {
+
+				x, err := strconv.Atoi(a)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				y, err := strconv.Atoi(b)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				z, err := strconv.Atoi(c)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if (x + y + z) == 2020 {
+					p := x * y * z
+					fmt.Printf("The three entries that sum to 2020 are %v, %v and %v. Multiplying them together produces %v * %v * %v = %v\n", x, y, z, x, y, z, p)
+
+					return
+				}
+			}
+		}
+	}
+}
---
+++ day2/day2.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	puzzle1()
+	puzzle2()
+}
+
+func puzzle1() {
+	fmt.Println("Day 2, puzzle 1.\n\n How many passwords are valid according to their policies?")
+
+	//Read file content in slice of bytes
+	content, err := ioutil.ReadFile("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/*
+		Cast slice of bytes to a string (which contains the whole file) and then
+		use the split function to create a slice using a separator (newline)
+	*/
+	lines := strings.Split(string(content), "\n")
+
+	/*
+		For each line, increment the counter if the password matches the policy.
+	*/
+	counter := 0
+	for _, line := range lines {
+
+		dashIndex := strings.Index(line, "-")
+		firstSpaceIndex := strings.Index(line, " ")
+		colonsIndex := strings.Index(line[firstSpaceIndex+1:], ":") + firstSpaceIndex + 1
+		secondSpaceIndex := strings.Index(line[colonsIndex+1:], " ") + colonsIndex + 1
+
+		lowerLimit, err := strconv.Atoi(line[:dashIndex])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		upperLimit, err := strconv.Atoi(line[dashIndex+1 : firstSpaceIndex])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c := line[firstSpaceIndex+1 : colonsIndex]
+		password := line[secondSpaceIndex+1:]
+		occurrences := strings.Count(password, c)
+
+		if occurrences >= lowerLimit && occurrences <= upperLimit {
+			counter++
+		}
+	}
+
+	fmt.Printf("There are %v valid passwords according to their policies\n", counter)
+
+}
+
+func puzzle2() {
+	fmt.Println("\nDay 2, puzzle 2.\n\n How many passwords are valid according to their policies?")
+
+	//Read file content in slice of bytes
+	content, err := ioutil.ReadFile("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/*
+		Cast slice of bytes to a string (which contains the whole file) and then
+		use the split function to create a slice using a separator (newline)
+	*/
+	lines := strings.Split(string(content), "\n")
+
+	/*
+		For each line, increment the validPasswords counter if the password matches the policy.
+	*/
+	validPasswords := 0
+	for _, line := range lines {
+
+		dashIndex := strings.Index(line, "-")
+		firstSpaceIndex := strings.Index(line, " ")
+		colonsIndex := strings.Index(line[firstSpaceIndex+1:], ":") + firstSpaceIndex + 1
+		secondSpaceIndex := strings.Index(line[colonsIndex+1:], " ") + colonsIndex + 1
+
+		firstPosition, err := strconv.Atoi(line[:dashIndex])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		secondPosition, err := strconv.Atoi(line[dashIndex+1 : firstSpaceIndex])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c := line[firstSpaceIndex+1 : colonsIndex]
+		password := line[secondSpaceIndex+1:]
+		correctPositionCounter := 0
+
+		if password[firstPosition-1:firstPosition] == c {
+			correctPositionCounter++
+		}
+
+		if password[secondPosition-1:secondPosition] == c {
+			correctPositionCounter++
+		}
+
+		if correctPositionCounter == 1 {
+			validPasswords++
+		}
+
+	}
+
+	fmt.Printf("There are %v valid passwords according to their policies\n", validPasswords)
+
+}
